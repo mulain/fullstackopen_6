0440ff7db613bebc1b0358652b1b14487abcd8c5
Wolf Arif Mardin <wolf.mardin@gmail.com>
2025-05-10 13:09:12 +0200

    finish 6.8

---
+++ anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from 'react-redux'
+
+// local
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
